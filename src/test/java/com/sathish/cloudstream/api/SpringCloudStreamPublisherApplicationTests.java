23e9c2174a98775fb765261a748588f9db3d9628
sathish1802 <sathish.k.gowda@gmail.com>
2021-02-03 15:26:18 +0530

    Initial commit

---
+++ src/test/java/com/sathish/cloudstream/api/SpringCloudStreamPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sathish.cloudstream.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
